b500ae0ed37ad72f90c1017a19ff4c73ec7a89dc
Aaron <aaronpena@gmail.com>
2022-08-16 23:24:55 -0700

    amazon_dropupdates

--- cypress/integration/2-advanced-examples/SegundaEvidencia.js
+++ cypress/integration/2-advanced-examples/SegundaEvidencia.js
@@ -1,6 +1,6 @@
 describe("Segunda evidencia ", function () {
   
-/*
+
   it("Encontrar y agregar una algo al carrito de compra", function () {
       //ingresamos a la pagina
       cy.visit("https://www.amazon.com/");
@@ -30,27 +30,29 @@ describe("Segunda evidencia ", function () {
           cy.get("@Carrito").click();
 
         });
-    });*/
+    });
 
   it("Verificar el dropdown y comprobar que los elementos existan.", function () {
     cy.visit("https://www.amazon.com/");
     cy.get('#nav-flyout-accountList').invoke('attr', 'style','display: block')
-    //cy.get('a[nav-title^="Your Lists"]').should('be.visible')
-    //cy.get('a[nav-al-title="Your Account"]').should('be.visible')
-    cy.get('a[nav-text^="Create a List"]').should('be.visible')
-    cy.get('a[nav-text="Find a List or Registry"]').should('be.visible')
-    cy.get('a[nav-text="AmazonSmile Charity Lists"]').should('be.visible')
-    cy.get('a[nav-text="Your Account"]').should('be.visible')
-    cy.get('a[nav-text="Account"]').should('be.visible')
-    cy.get('a[nav-text="Orders"]').should('be.visible')
-    cy.get('a[nav-text="Recommendations"]').should('be.visible')
-    cy.get('a[nav-text="Browsing History"]').should('be.visible')
-    cy.get('a[nav-text="Watchlist"]').should('be.visible')
-    cy.get('a[nav-text="Video Purchases & Rentals"]').should('be.visible')
-    cy.get('a[nav-text="Kindle Unlimited"]').should('be.visible')
-    cy.get('a[nav-text="Content & Devices"]').should('be.visible')
-    cy.get('a[nav-text="Subscribe & Save Items"]').should('be.visible')
-    cy.get('a[nav-text="Memberships & Subscriptions"]').should('be.visible')
-    cy.get('a[nav-text="Music Library"]').should('be.visible')
+
+    cy.get('#nav-al-wishlist > a:nth-child(2) > span').should('be.visible')
+cy.get('#nav-al-wishlist > a:nth-child(3) > span').should('be.visible')
+cy.get('#nav-al-wishlist > a:nth-child(4) > span').should('be.visible')
+cy.get('#nav-al-your-account > a:nth-child(2) > span').should('be.visible')
+cy.get('#nav-al-your-account > a:nth-child(3) > span').should('be.visible')
+cy.get('#nav-al-your-account > a:nth-child(4) > span').should('be.visible')
+cy.get('#nav-al-your-account > a:nth-child(5) > span').should('be.visible')
+cy.get('#nav-al-your-account > a:nth-child(6) > span').should('be.visible')
+cy.get('#nav-al-your-account > a:nth-child(7) > span').should('be.visible')
+cy.get('#nav-al-your-account > a:nth-child(8) > span').should('be.visible')
+cy.get('#nav-al-your-account > a:nth-child(9) > span').should('be.visible')
+cy.get('#nav-al-your-account > a:nth-child(10) > span').should('be.visible')
+cy.get('#nav-al-your-account > a:nth-child(11) > span').should('be.visible')
+
+
+
+
   });
+
 });
