d58a9d5d8cc3ab5ca0cb0371b1c04ed2248c71da
Aaron <aaronpena@gmail.com>
2022-08-16 19:38:17 -0700

    acippopshop

--- cypress/integration/2-advanced-examples/SegundaEvidencia.js
+++ cypress/integration/2-advanced-examples/SegundaEvidencia.js
@@ -4,22 +4,26 @@ describe("Segunda practica ",function(){
 it('Encontrar y agregar una camisa desde la pagina de inicio', function(){
      cy.visit("acid.com.mx") 
 //conseguir contenedor
-cy.get('.grid-row-collection > .widget-product > .products-grid > .slick-list > .slick-track').as('Camisas')
+cy.get('#featured-products-165661793050c01cd4 > .container > :nth-child(1) > .grid-row-collection > .widget-product > .products-grid').as('Camisas')
 cy.get('@Camisas')
-        .find('.product-name')
+
+        .find('.product-title')
         .each(($el,index, $list) => {
-            cy.get('@Camisas').eq(index).find('.price').then(function($el1){
+            cy.get('@Camisas').find('.price-box').then(function($el1){
                 let precio = $el1.text()
                 cy.log(precio)
-            
-            if($el.attr('title') === 'Playera ' && precio.includes('299.00')){
+        //////////////////////
+            if($el.attr('.product-bottom') === 'Playera Nanana Anya ' && precio.includes('299.00')){
                 cy.log('Se ha encontrado el elemento buscado')
                 cy.log('Se ha encontrado el precio buscado')
-                cy.get('@Camisas').eq(index).contains('Add to cart').click()
+                
+                cy.get('@Camisas').eq(index).contains('Agregar +').click()
             }
         })
         
     })
+
+    //cy.get('#acid-pop > div.ajax-success-normal.ajax-success-modal.halo-modal').should('contain.text','se agrega a tu carrito.').should('be.visible')
 })
 it('Verificar el dropdown y comprobar que los elementos existan.',function(){
 
